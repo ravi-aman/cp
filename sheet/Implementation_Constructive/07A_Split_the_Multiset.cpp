aa9fe17ea54c8e7f75eb2beb4197435784f9eb95
Ravikant Tiwary <tiwariravikant04@gmail.com>
2024-07-17 20:59:43 +0530

    fox and snake

---
+++ sheet/Implementation_Constructive/07A_Split_the_Multiset.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int operation(int n, int k)
+{
+    int result = 0;
+    
+
+
+    return result;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    pair<int, int> number;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> number.first;
+        cin >> number.second;
+    }
+    for (int i = 0; i < t; i++)
+    {
+        cout << operation(number.first, number.second);
+        cout << endl;
+    }
+
+    return 0;
+}
